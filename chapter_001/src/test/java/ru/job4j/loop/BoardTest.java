337a9fc2b56d6a9828bcdf7f56235e390f71babf
AMKir1 <andykirill@gmail.com>
2017-07-01 23:26:16 +0300

    4.3. Построить шахматную доску в псевдографике.[#17962]

--- chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -15,7 +15,7 @@ public class BoardTest {
 	public void whenWidthEqualsHeigh() {
 		Board board = new Board();
 		String result = board.paint(3, 3);
-		String expected = "x x" + System.getProperty("line.separator") + " x " + System.getProperty("line.separator") + "x x" + System.getProperty("line.separator");
+		String expected = String.format("x x%<s x %<sx x%<s", System.getProperty("line.separator"));
 		assertThat(result, is(expected));
 	}
 	/**
@@ -24,7 +24,7 @@ public class BoardTest {
 	public void whenWidthDoNotEqualsHeigh() {
 		Board board = new Board();
 		String result = board.paint(5, 4);
-		String expected = "x x x" + System.getProperty("line.separator") + " x x " + System.getProperty("line.separator") + "x x x" + System.getProperty("line.separator") + " x x " + System.getProperty("line.separator");
+		String expected = String.format("x x x%<s x x %<sx x x%<s x x %<s", System.getProperty("line.separator"));
 		assertThat(result, is(expected));
 		}
 }
