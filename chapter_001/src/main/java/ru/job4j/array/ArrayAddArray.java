36583b519dcd4bb0de763e33cacbb29fe4a721bc
AMKir1 <andykirill@gmail.com>
2017-07-28 22:57:37 +0300

    Объединение двух массивов исправленный вариант

--- chapter_001/src/main/java/ru/job4j/array/ArrayAddArray.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayAddArray.java
@@ -10,24 +10,16 @@ public class ArrayAddArray {
 	/**
 	* Duplicate.
 	* @param array1 - first args.
-	* @param array2 - first args.
+	* @param array2 - second args.
 	* @return result.
 	*/
     public int[] sumArrays(int[] array1, int[] array2) {
-		int length1 = 0;
-		int length2 = 0;
-		for (int i : array1) {
-			length1 += 1;
-		}
-		for (int i : array2) {
-			length2 += 1;
-		}
-		int[] array3 = new int[length1 + length2];
+		int[] array3 = new int[array1.length + array2.length];
 		for (int i = 0; i < array3.length; i++) {
-			if (i < length1) {
+			if (i < array1.length) {
 				array3[i] = array1[i];
 			} else {
-				array3[i] = array2[i - length1];
+				array3[i] = array2[i - array1.length];
 			}
 		}
 		return array3;
