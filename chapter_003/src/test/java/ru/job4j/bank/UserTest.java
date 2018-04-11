d6cd40dd336998ef61646b64dc12b8ad9cfbd966
AMKir1 <andykirill@gmail.com>
2018-04-11 16:19:41 +0300

    bank 2

---
+++ chapter_003/src/test/java/ru/job4j/bank/UserTest.java
@@ -0,0 +1,12 @@
+package ru.job4j.bank;
+
+/**
+ * Chapter_003. Collection. Lite.
+ * Task: Конвертация двумерного массива в ArrayList и наоборот [#10035]
+ * @author Andrei Kirillovykh (mailto:andykirill@gmail.com)
+ * @version 1
+ */
+
+public class UserTest {
+
+}
