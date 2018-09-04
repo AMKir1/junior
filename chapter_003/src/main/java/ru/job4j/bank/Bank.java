aa5285991b3d6091c2d7eb65c3b5e51121a6bdb1
Кирилловых <andykirill@gmail.com>
2018-09-04 11:17:01 +0300

    Upgrade Bank v.2.0

--- chapter_003/src/main/java/ru/job4j/bank/Bank.java
+++ chapter_003/src/main/java/ru/job4j/bank/Bank.java
@@ -92,7 +92,7 @@ public class Bank {
                 accTo.setValue(accTo.getValue() + amount);
             }
         }
-        return status && true;
+        return status;
     }
     /**
      * Поиск пользователя по паспорту.
