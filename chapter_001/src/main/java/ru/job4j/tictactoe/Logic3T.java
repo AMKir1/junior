e6cea6a03972969f6f0019a3c8b7bf2f5ff52f72
Andrei Kirillovykh <andykirill@gmail.com>
2019-11-01 15:13:28 +0300

    Trevis CI не видит javaFX 3

--- chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -1,70 +1,70 @@
-package ru.job4j.tictactoe;
-
-class Logic3T {
-    private final Figure3T[][] table;
-
-    Logic3T(Figure3T[][] table) {
-        this.table = table;
-    }
-
-    boolean isWinnerX() {
-        boolean winHorizontal, winVertical, winLeftDiagonal, winRightDiagonal;
-        winHorizontal = true;
-        winVertical = true;
-        winLeftDiagonal = true;
-        winRightDiagonal = true;
-        for (int i = 0; i < table.length; i++) {
-            for (int j = 0; j < table.length; j++) {
-                if (i == j) {
-                    if (!(winLeftDiagonal && table[i][j].hasMarkX())) {
-                        winLeftDiagonal = false;
-                    }
-                }
-                if (!(winRightDiagonal && table[i][table.length - 1 - j].hasMarkX())) {
-                    winRightDiagonal = false;
-                }
-                if (!(winHorizontal && table[i][j].hasMarkX())) {
-                    winHorizontal = false;
-                }
-                if (!(winVertical && table[j][i].hasMarkX())) {
-                    winVertical = false;
-                }
-
-            }
-        }
-        return winHorizontal || winVertical || winLeftDiagonal || winRightDiagonal;
-    }
-
-    boolean isWinnerO() {
-        boolean winHorizontal, winVertical, winLeftDiagonal, winRightDiagonal;
-        winHorizontal = true;
-        winVertical = true;
-        winLeftDiagonal = true;
-        winRightDiagonal = true;
-        for (int i = 0; i < table.length; i++) {
-            for (int j = 0; j < table.length; j++) {
-                if (i == j) { // главная диагональ
-                    if (!(winLeftDiagonal && table[i][j].hasMarkO())) {
-                        winLeftDiagonal = false;
-                    }
-                }
-                if (j == table.length - 1 - i) { // побочная диагональ
-                    if (!(winRightDiagonal && table[i][j].hasMarkO())) {
-                        winRightDiagonal = false;
-                    }
-                }
-                if (!(winHorizontal && table[i][j].hasMarkO())) {
-                    winHorizontal = false;
-                }
-                if (!(winVertical && table[j][i].hasMarkO())) {
-                    winVertical = false;
-                }
-            }
-        }
-        return winHorizontal || winVertical || winLeftDiagonal || winRightDiagonal;
-    }
-
-    boolean hasGap() {
-       return !isWinnerX() && !isWinnerO();
-    }
-}
+//package ru.job4j.tictactoe;
+//
+//class Logic3T {
+//    private final Figure3T[][] table;
+//
+//    Logic3T(Figure3T[][] table) {
+//        this.table = table;
+//    }
+//
+//    boolean isWinnerX() {
+//        boolean winHorizontal, winVertical, winLeftDiagonal, winRightDiagonal;
+//        winHorizontal = true;
+//        winVertical = true;
+//        winLeftDiagonal = true;
+//        winRightDiagonal = true;
+//        for (int i = 0; i < table.length; i++) {
+//            for (int j = 0; j < table.length; j++) {
+//                if (i == j) {
+//                    if (!(winLeftDiagonal && table[i][j].hasMarkX())) {
+//                        winLeftDiagonal = false;
+//                    }
+//                }
+//                if (!(winRightDiagonal && table[i][table.length - 1 - j].hasMarkX())) {
+//                    winRightDiagonal = false;
+//                }
+//                if (!(winHorizontal && table[i][j].hasMarkX())) {
+//                    winHorizontal = false;
+//                }
+//                if (!(winVertical && table[j][i].hasMarkX())) {
+//                    winVertical = false;
+//                }
+//
+//            }
+//        }
+//        return winHorizontal || winVertical || winLeftDiagonal || winRightDiagonal;
+//    }
+//
+//    boolean isWinnerO() {
+//        boolean winHorizontal, winVertical, winLeftDiagonal, winRightDiagonal;
+//        winHorizontal = true;
+//        winVertical = true;
+//        winLeftDiagonal = true;
+//        winRightDiagonal = true;
+//        for (int i = 0; i < table.length; i++) {
+//            for (int j = 0; j < table.length; j++) {
+//                if (i == j) { // главная диагональ
+//                    if (!(winLeftDiagonal && table[i][j].hasMarkO())) {
+//                        winLeftDiagonal = false;
+//                    }
+//                }
+//                if (j == table.length - 1 - i) { // побочная диагональ
+//                    if (!(winRightDiagonal && table[i][j].hasMarkO())) {
+//                        winRightDiagonal = false;
+//                    }
+//                }
+//                if (!(winHorizontal && table[i][j].hasMarkO())) {
+//                    winHorizontal = false;
+//                }
+//                if (!(winVertical && table[j][i].hasMarkO())) {
+//                    winVertical = false;
+//                }
+//            }
+//        }
+//        return winHorizontal || winVertical || winLeftDiagonal || winRightDiagonal;
+//    }
+//
+//    boolean hasGap() {
+//       return !isWinnerX() && !isWinnerO();
+//    }
+//}
