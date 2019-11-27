025f03ffc0f34852c9f0de6f4c9b19481c7b3a29
Andrei Kirillovykh <andykirill@gmail.com>
2019-11-27 11:43:01 +0300

    Крестики-нолики на JavaFX [#49777]

--- chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -1,70 +1,83 @@
-//package ru.job4j.tictactoe;
-//
-//class Logic3T {
-//    private final Figure3T[][] table;
-//
-//    Logic3T(Figure3T[][] table) {
-//        this.table = table;
-//    }
-//
-//    boolean isWinnerX() {
-//        boolean winHorizontal, winVertical, winLeftDiagonal, winRightDiagonal;
-//        winHorizontal = true;
-//        winVertical = true;
-//        winLeftDiagonal = true;
-//        winRightDiagonal = true;
-//        for (int i = 0; i < table.length; i++) {
-//            for (int j = 0; j < table.length; j++) {
-//                if (i == j) {
-//                    if (!(winLeftDiagonal && table[i][j].hasMarkX())) {
-//                        winLeftDiagonal = false;
-//                    }
-//                }
-//                if (!(winRightDiagonal && table[i][table.length - 1 - j].hasMarkX())) {
-//                    winRightDiagonal = false;
-//                }
-//                if (!(winHorizontal && table[i][j].hasMarkX())) {
-//                    winHorizontal = false;
-//                }
-//                if (!(winVertical && table[j][i].hasMarkX())) {
-//                    winVertical = false;
-//                }
-//
-//            }
-//        }
-//        return winHorizontal || winVertical || winLeftDiagonal || winRightDiagonal;
-//    }
-//
-//    boolean isWinnerO() {
-//        boolean winHorizontal, winVertical, winLeftDiagonal, winRightDiagonal;
-//        winHorizontal = true;
-//        winVertical = true;
-//        winLeftDiagonal = true;
-//        winRightDiagonal = true;
-//        for (int i = 0; i < table.length; i++) {
-//            for (int j = 0; j < table.length; j++) {
-//                if (i == j) { // главная диагональ
-//                    if (!(winLeftDiagonal && table[i][j].hasMarkO())) {
-//                        winLeftDiagonal = false;
-//                    }
-//                }
-//                if (j == table.length - 1 - i) { // побочная диагональ
-//                    if (!(winRightDiagonal && table[i][j].hasMarkO())) {
-//                        winRightDiagonal = false;
-//                    }
-//                }
-//                if (!(winHorizontal && table[i][j].hasMarkO())) {
-//                    winHorizontal = false;
-//                }
-//                if (!(winVertical && table[j][i].hasMarkO())) {
-//                    winVertical = false;
-//                }
-//            }
-//        }
-//        return winHorizontal || winVertical || winLeftDiagonal || winRightDiagonal;
-//    }
-//
-//    boolean hasGap() {
-//       return !isWinnerX() && !isWinnerO();
-//    }
-//}
+package ru.job4j.tictactoe;
+
+import java.util.function.Predicate;
+
+class Logic3T {
+    private final Figure3T[][] table;
+
+    Logic3T(Figure3T[][] table) {
+        this.table = table;
+    }
+
+    public boolean fillBy(Predicate<Figure3T> predicate, int startX, int startY, int deltaX, int deltaY) {
+        boolean result = true;
+        for (int index = 0; index != this.table.length; index++) {
+            Figure3T cell = this.table[startX][startY];
+            startX += deltaX;
+            startY += deltaY;
+            if (!predicate.test(cell)) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    boolean isWinnerX() {
+                //horizontal left-right
+        return this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 0)
+                || this.fillBy(Figure3T::hasMarkX, 0, 1, 1, 0)
+                || this.fillBy(Figure3T::hasMarkX, 0, this.table.length - 1, 1, 0)
+                //horizontal right-left
+                || this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, -1, 0)
+                || this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 1, -1, 0)
+                || this.fillBy(Figure3T::hasMarkX, this.table.length - 1, this.table.length - 1, -1, 0)
+                //vertical top-bottom
+                || this.fillBy(Figure3T::hasMarkX, 0, 0, 0, 1)
+                || this.fillBy(Figure3T::hasMarkX, 1, 0, 0, 1)
+                || this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, 0, 1)
+                //vertical bottom-top
+                || this.fillBy(Figure3T::hasMarkX, 0, this.table.length - 1, 0, -1)
+                || this.fillBy(Figure3T::hasMarkX, 1, this.table.length - 1, 0, -1)
+                || this.fillBy(Figure3T::hasMarkX, this.table.length - 1, this.table.length - 1, 0, -1)
+                //horizontal top-to-bottom-right
+                || this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1)
+                //horizontal top-to-bottom-left
+                || this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, -1, 1)
+                //horizontal bottom-to-top-right
+                || this.fillBy(Figure3T::hasMarkX, 0, this.table.length - 1, 1, -1)
+                //horizontal bottom-to-top-left
+                || this.fillBy(Figure3T::hasMarkX, this.table.length - 1, this.table.length - 1, -1, -1);
+    }
+
+    boolean isWinnerO() {
+                //horizontal left-right
+        return this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 0)
+                || this.fillBy(Figure3T::hasMarkO, 0, 1, 1, 0)
+                || this.fillBy(Figure3T::hasMarkO, 0, this.table.length - 1, 1, 0)
+                //horizontal right-left
+                || this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 0)
+                || this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 1, -1, 0)
+                || this.fillBy(Figure3T::hasMarkO, this.table.length - 1, this.table.length - 1, -1, 0)
+                //vertical top-bottom
+                || this.fillBy(Figure3T::hasMarkO, 0, 0, 0, 1)
+                || this.fillBy(Figure3T::hasMarkO, 1, 0, 0, 1)
+                || this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, 0, 1)
+                //vertical bottom-top
+                || this.fillBy(Figure3T::hasMarkO, 0, this.table.length - 1, 0, -1)
+                || this.fillBy(Figure3T::hasMarkO, 1, this.table.length - 1, 0, -1)
+                || this.fillBy(Figure3T::hasMarkO, this.table.length - 1, this.table.length - 1, 0, -1)
+                //horizontal top-to-bottom-right
+                || this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 1)
+                //horizontal top-to-bottom-left
+                || this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 1)
+                //horizontal bottom-to-top-right
+                || this.fillBy(Figure3T::hasMarkO, 0, this.table.length - 1, 1, -1)
+                //horizontal bottom-to-top-left
+                || this.fillBy(Figure3T::hasMarkO, this.table.length - 1, this.table.length - 1, -1, -1);
+    }
+
+    boolean hasGap() {
+       return !isWinnerX() && !isWinnerO();
+    }
+}
