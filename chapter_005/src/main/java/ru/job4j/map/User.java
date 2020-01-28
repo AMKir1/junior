64027a56d9b433c7a1a965b6ce880ccffc413b93
Andrei Kirillovykh <andykirill@gmail.com>
2020-01-28 08:23:29 +0300

    1. Создать модель User [#999]

---
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,45 @@
+package ru.job4j.map;
+/*
+ * Chapter_005. Collections. Pro.[#146]
+ * Task: 1. Создать модель User [#999]
+ * @author Andrei Kirillovykh (mailto:andykirill@gmail.com)
+ * @version 1
+ */
+
+import java.util.Calendar;
+
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Calendar birthday) {
+        this.birthday = birthday;
+    }
+}
