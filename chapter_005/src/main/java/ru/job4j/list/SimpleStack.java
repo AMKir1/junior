0678c3aeb5c10bd11c4855aea978c2f4ecde5e62
Andrei Kirillovykh <andykirill@gmail.com>
2020-01-31 15:48:06 +0300

    5.3.3. Используя контейнер на базе связанного списка создать контейнер Stack [#71474] v5

--- chapter_005/src/main/java/ru/job4j/list/SimpleStack.java
+++ chapter_005/src/main/java/ru/job4j/list/SimpleStack.java
@@ -4,7 +4,7 @@ package ru.job4j.list;
  * Chapter_005. Collections. Pro.[#146]
  * Task: 5.3.3. Используя контейнер на базе связанного списка создать контейнер Stack [#71474]
  * @author Andrei Kirillovykh (mailto:andykirill@gmail.com)
- * @version 4
+ * @version 5
  */
 public class SimpleStack<T> {
 
@@ -12,7 +12,7 @@ public class SimpleStack<T> {
     private SimpleLinkedList<T> collection = new SimpleLinkedList<T>();
 
     public <T> T poll() {
-        return (T) collection.remove();
+        return collection.size() == 0 ? null : (T) collection.remove();
     }
 
     public void push(T value) {
