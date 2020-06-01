ae55dbcd29836668d90dc7a6bee1dbc34829ad3d
Andrei Kirillovykh <andykirill@gmail.com>
2020-06-01 18:35:50 +0300

    Проверить две строчки на идентичность. [#273008]

--- chapter_005/src/main/java/ru/job4j/collectioin/FreezeStr.java
+++ chapter_005/src/main/java/ru/job4j/collectioin/FreezeStr.java
@@ -3,9 +3,10 @@ package ru.job4j.collectioin;
  * Chapter_005. Collections. Pro.[#146]
  * Task: Проверить две строчки на идентичность. [#273008]
  * @author Andrei Kirillovykh (mailto:andykirill@gmail.com)
- * @version 1
+ * @version 2
  */
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class FreezeStr {
     public static boolean eq(String left, String right) {
@@ -14,15 +15,20 @@ public class FreezeStr {
         } else {
             char[] chLeft = left.toCharArray();
             char[] chRight = right.toCharArray();
+
             Arrays.sort(chLeft); //o(n log (n))
             Arrays.sort(chRight); //o(n log (n))
 
-            for (int i = 0; i < chLeft.length; i++) { //o(n)
-                if (chLeft[i] != chRight[i]) {
-                    return false;
-                }
+            HashMap<Character, Integer> map1 = new HashMap<>();
+            HashMap<Character, Integer> map2 = new HashMap<>();
+
+            for(int i = 0; i < chLeft.length; i++){
+                map1.put(chLeft[i], i);
+                map2.put(chRight[i], i);
             }
-            return true;
+
+            return map1.equals(map2);
         }
     }
+
 }
