70b17d642026b25dfe2e843b4a8d25996395ba66
Andrei Kirillovykh <andykirill@gmail.com>
2020-06-05 13:24:21 +0300

    2. Статистику по коллекции. [#45889] v4

--- chapter_005/src/main/java/ru/job4j/collectioin/Analize.java
+++ chapter_005/src/main/java/ru/job4j/collectioin/Analize.java
@@ -3,7 +3,7 @@ package ru.job4j.collectioin;
  * Chapter_005. Collections. Pro.[#146]
  * Task: 2. Статистику по коллекции. [#45889]
  * @author Andrei Kirillovykh (mailto:andykirill@gmail.com)
- * @version 3
+ * @version 4
  */
 import java.util.HashMap;
 import java.util.List;
@@ -14,8 +14,29 @@ import java.util.stream.Collectors;
 public class Analize {
     public Info diff(List<User> previous, List<User> current) {
 
-        Map<Integer, String> currentMap = current.stream().collect(Collectors.toMap(u -> u.id, u -> u.name));
+        Map<Integer, String> previoustMap = previous.stream().collect(Collectors.toMap(u -> u.id, u -> u.name));
 
+        Info info = new Info(0, 0, 0);
+
+        for (User u : current) {
+            String res = previoustMap.remove(u.id);
+            if (res == null) {
+                info.added++;
+            }
+
+            if ( res != null && res != u.name) {
+                info.changed++;
+            }
+        }
+
+        info.deleted = previoustMap.size();
+
+        return info;
+    }
+
+    /*   public Info diff(List<User> previous, List<User> current) {
+
+        Map<Integer, String> currentMap = current.stream().collect(Collectors.toMap(u -> u.id, u -> u.name));
 
         Info info = new Info(0, 0, 0);
         int diffSize = previous.size() - current.size();
@@ -23,18 +44,20 @@ public class Analize {
         info.added = diffSize < 0 ? Math.abs(diffSize) : 0;
         info.deleted = diffSize > 0 ? diffSize : 0;
 
-//        for (User up : previous) {
-//            for (User uc : current) {
-//                if (up.equals(uc)) {
-//                    noAddDel++;
-//                } else {
-//                    if (up.id == uc.id && !up.name.equals(uc.name)) {
-//                        noAddDel++;
-//                        info.changed++;
-//                    }
-//                }
-//            }
-//        }
+
+        //        for (User up : previous) {
+        //            for (User uc : current) {
+        //                if (up.equals(uc)) {
+        //                    noAddDel++;
+        //                } else {
+        //                    if (up.id == uc.id && !up.name.equals(uc.name)) {
+        //                        noAddDel++;
+        //                        info.changed++;
+        //                    }
+        //                }
+        //            }
+        //        }
+
 
         for (User up : previous) {
             if (currentMap.containsKey(up.id)) {
@@ -51,8 +74,9 @@ public class Analize {
                 info.added += info.deleted;
             }
         }
+
         return info;
-    }
+    }*/
 
     public static class User {
         int id;
