40539baa7e0308559767fdf6357bd44ee11eec52
Andrei Kirillovykh <andykirill@gmail.com>
2020-06-22 12:05:43 +0300

    5. Валидация параметров запуска. v3

--- chapter_006/src/main/java/ru/job4j/io/Search.java
+++ chapter_006/src/main/java/ru/job4j/io/Search.java
@@ -3,7 +3,7 @@ package ru.job4j.io;
  * Chapter_006. Ввод-вывод[#633]
  * Task: 4.1. Сканирование файловой системы. [#106929]
  * Task: 3.0. Тестирование IO [#173905]
- * Task: 5. Валидация параметров запуска. [#246865] v2
+ * Task: 5. Валидация параметров запуска. [#246865] v3
  * @author Andrei Kirillovykh (mailto:andykirill@gmail.com)
  * @version 1
  */
@@ -16,10 +16,17 @@ import java.util.List;
 public class Search  {
     public static void main(String[] args) throws IOException {
         if (args.length == 0) {
-            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+            throw new IllegalArgumentException("Root folder is null.");
+        } else {
+            try {
+                String ext = args[1];
+                File start = new File(args[0]);
+                search(start, ext).forEach(System.out::println);
+            } catch (Exception e) {
+                e.getMessage();
+            }
+
         }
-        File start = new File(args[0]);
-        search(start, "java").forEach(System.out::println);
     }
 
     public static List<Path> search(File root, String ext) throws IOException {
