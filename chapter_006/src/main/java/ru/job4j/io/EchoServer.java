298880c60a3afe21075eea90134b6ca256a9067a
Andrei Kirillovykh <andykirill@gmail.com>
2020-07-16 15:24:30 +0300

    1. Бот [#7921]

--- chapter_006/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_006/src/main/java/ru/job4j/io/EchoServer.java
@@ -2,9 +2,11 @@ package ru.job4j.io;
 /*
  * Chapter_006. Ввод-вывод[#633]
  * Task: 0. Что такое Socket? [#4850]
+ * Task: 1. Бот [#7921]
  * @author Andrei Kirillovykh (mailto:andykirill@gmail.com)
  * @version 1
  */
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -18,13 +20,21 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str;
-                    while (!(str = in.readLine() ).isEmpty()) {
-                        if (str.contains("bye")) {
-                            server.close();
+                    String msg = "";
+                    while (!(str = in.readLine()).isEmpty()) {
+                        if (str.contains("msg")) {
+                            if (str.contains("Exit")) {
+                                server.close();
+                            } else if (str.contains("Hello")) {
+                                msg = "Hello, dear friend.";
+                            } else {
+                                msg = str.split("=")[1].split(" ")[0];
+                            }
                         }
                         System.out.println(str);
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write(msg.getBytes());
                 }
             }
         }
