ff65316f23aef09bdc2c930a1818baf7730dd473
Andrei Kirillovykh <andykirill@gmail.com>
2020-07-16 16:14:03 +0300

    3. Slf4j - вывод переменных. [#268852]

--- chapter_006/src/main/java/ru/job4j/io/UsageLog4j.java
+++ chapter_006/src/main/java/ru/job4j/io/UsageLog4j.java
@@ -3,9 +3,11 @@ package ru.job4j.io;
  * Chapter_006. Ввод-вывод[#633]
  * Task: 1. Log4j. Логирование системы. [#95335]
  * Task: 2. Simple Loggin Facade 4 Java. [#268849]
+ * Task: 3. Slf4j - вывод переменных. [#268852]
  * @author Andrei Kirillovykh (mailto:andykirill@gmail.com)
  * @version 1
  */
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,10 +16,15 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        byte value = 2;
+        short age = 25;
+        int height = 177;
+        long weight = 80L;
+        float salary = 2.5f;
+        double cinema = 7D;
+        boolean man = true;
+        char symbol = '⅞';
+
+        LOG.debug("Info value : {}, age : {}, height : {}, weight : {}, salary : {}, cinema : {}, man : {}, symbol : {}", value, age, height, weight, salary, cinema, man, symbol);
     }
 }
