4e35bd37b9066d7f087f2842f88bdf9dbfd35cbe
Andrei Kirillovykh <andykirill@gmail.com>
2020-12-24 16:00:17 +0300

    Принципы Kiss, Dry и Yagni [#238813] v2

--- chapter_009/src/main/java/ru/job4j/kiss/MaxMin.java
+++ chapter_009/src/main/java/ru/job4j/kiss/MaxMin.java
@@ -4,26 +4,27 @@ package ru.job4j.kiss;
  * Task: 1. Принципы Kiss, Dry и Yagni [#238813]
  * Разработайте класс для поиска максимального и минимального элемента по критерию java.util.Comparator.
  * @author Andrei Kirillovykh (mailto:andykirill@gmail.com)
- * @version 1
+ * @version 2
  */
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class MaxMin {
 
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        T max = value.get(0);
-        for (int i = 0; i < value.size() - 1; i++) {
-            max = comparator.compare(max, value.get(i + 1)) >= 0  ? max : value.get(i + 1);
-        }
-        return max;
+        return getValue(value, comparator, x -> x >= 0);
     }
 
     public <T> T min(List<T> value, Comparator<T> comparator) {
-        T min = value.get(0);
+        return getValue(value, comparator, x -> x <= 0);
+    }
+
+    public <T> T getValue(List<T> value, Comparator<T> comparator, Predicate<Integer> predicate) {
+        T res = value.get(0);
         for (int i = 0; i < value.size() - 1; i++) {
-            min = comparator.compare(min, value.get(i + 1)) <= 0  ? min : value.get(i + 1);
+            res = predicate.test(comparator.compare(res, value.get(i + 1))) ? res : value.get(i + 1);
         }
-        return min;
+        return res;
     }
 }
