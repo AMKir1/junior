c80833103d316fc3e419e6785c503cc79e496bd0
Andrei Kirillovykh <andykirill@gmail.com>
2020-12-24 13:49:55 +0300

    fix +1

---
+++ chapter_009/src/main/java/ru/job4j/srp/StartEnglish.java
@@ -0,0 +1,15 @@
+package ru.job4j.srp;
+
+public class StartEnglish {
+
+    /**
+     * Method of starting the program.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        EnglishExercises ee = new EnglishExercises();
+        System.out.println("Start to learn English!");
+
+    }
+
+}
