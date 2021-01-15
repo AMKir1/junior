c71cc41177f93398c180ca8a43c2688fcf00c670
Andrei Kirillovykh <andykirill@gmail.com>
2021-01-15 11:52:02 +0300

    Парковка машин [#853]

--- chapter_009/src/main/java/ru/job4j/parking/Vehicle.java
+++ chapter_009/src/main/java/ru/job4j/parking/Vehicle.java
@@ -3,12 +3,12 @@ package ru.job4j.parking;
  * Chapter_009. OOD [#143]
  * Task: 2. Парковка машин [#853]
  * @author Andrei Kirillovykh (mailto:andykirill@gmail.com)
- * @version 1
+ * @version 2
  */
 /**
  * Vehicle class.
  */
-public class Vehicle {
+public abstract class Vehicle {
 
     /**
      * Vehicle's name.
