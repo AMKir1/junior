df00315f51bd181a4510bcfb08fbfdbcfd18d3e2
Andrei Kirillovykh <andykirill@gmail.com>
2021-01-23 15:43:22 +0300

    1.1. Состояние нити.[229175#453891] v2

--- chapter_010/src/main/java/ru/job4j/concurrent/ThreadState.java
+++ chapter_010/src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -3,24 +3,26 @@ package ru.job4j.concurrent;
  * Chapter_010. 1. Multithreading[171#453877].
  * Task: 1.1. Состояние нити.[229175#453891].
  * @author Andrei Kirillovykh (mailto:andykirill@gmail.com).
- * @version 1.
+ * @version 2.
  */
 public class ThreadState {
     public static void main(String[] args) {
         Thread first = new Thread(() -> System.out.println(Thread.currentThread().getName()));
         Thread second = new Thread(() -> System.out.println(Thread.currentThread().getName()));
-        System.out.println("First Thread: " + first.getState());
-        System.out.println("Second Thread: " + second.getState());
+        System.out.println(first.getName() + " : "  + first.getState());
+        System.out.println(second.getName() + " : "  + second.getState());
         first.start();
         second.start();
-        while (first.getState() != Thread.State.TERMINATED) {
-            System.out.println("First Thread: " + first.getState());
-        }
-        while (second.getState() != Thread.State.TERMINATED) {
-            System.out.println("Second Thread: " + second.getState());
-        }
-        System.out.println("First Thread: " + first.getState());
-        System.out.println("Second Thread: " + second.getState());
+        printNotTerminatedState(first);
+        printNotTerminatedState(second);
+        System.out.println(first.getName() + " : "  + first.getState());
+        System.out.println(second.getName() + " : "  + second.getState());
         System.out.println("fin!");
+    }
+
+    private static void printNotTerminatedState(Thread thread) {
+        while (thread.getState() != Thread.State.TERMINATED) {
+            System.out.println(thread.getName() + " : "  + thread.getState());
+        }
     }
 }
