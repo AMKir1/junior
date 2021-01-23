17a9406896347cd9e3c6b533117d957510a45124
Andrei Kirillovykh <andykirill@gmail.com>
2021-01-23 17:02:34 +0300

    1.1. Состояние нити.[229175#453891]. v3

--- chapter_010/src/main/java/ru/job4j/concurrent/ThreadState.java
+++ chapter_010/src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -3,7 +3,7 @@ package ru.job4j.concurrent;
  * Chapter_010. 1. Multithreading[171#453877].
  * Task: 1.1. Состояние нити.[229175#453891].
  * @author Andrei Kirillovykh (mailto:andykirill@gmail.com).
- * @version 2.
+ * @version 3.
  */
 public class ThreadState {
     public static void main(String[] args) {
@@ -13,16 +13,16 @@ public class ThreadState {
         System.out.println(second.getName() + " : "  + second.getState());
         first.start();
         second.start();
-        printNotTerminatedState(first);
-        printNotTerminatedState(second);
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
+            if (first.getState() != Thread.State.TERMINATED) {
+                System.out.println(first.getName() + " : " + first.getState());
+            }
+            if (second.getState() != Thread.State.TERMINATED) {
+                System.out.println(second.getName() + " : " + second.getState());
+            }
+        }
         System.out.println(first.getName() + " : "  + first.getState());
         System.out.println(second.getName() + " : "  + second.getState());
         System.out.println("fin!");
-    }
-
-    private static void printNotTerminatedState(Thread thread) {
-        while (thread.getState() != Thread.State.TERMINATED) {
-            System.out.println(thread.getName() + " : "  + thread.getState());
-        }
     }
 }
