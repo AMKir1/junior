cf8a824719571d0e5571f99e334188eefba26682
Andrei Kirillovykh <andykirill@gmail.com>
2021-01-25 10:52:51 +0300

    3. Класс хранилища пользователей UserStorage[1104#453906]. v3

--- chapter_010/src/main/java/ru/job4j/jcip/UserStorage.java
+++ chapter_010/src/main/java/ru/job4j/jcip/UserStorage.java
@@ -3,7 +3,7 @@ package ru.job4j.jcip;
  * Chapter_010. 1. Multithreading[171#453877].
  * Task: 3. Класс хранилища пользователей UserStorage[1104#453906].
  * @author Andrei Kirillovykh (mailto:andykirill@gmail.com).
- * @version 2.
+ * @version 3.
  */
 
 import net.jcip.annotations.GuardedBy;
@@ -15,32 +15,27 @@ import java.util.Map;
 @ThreadSafe
 public class UserStorage {
     @GuardedBy("this")
-    private Map<Integer, User> userStorage = new HashMap<>();
+    private final Map<Integer, User> userStorage = new HashMap<>();
 
     public synchronized boolean add(User user) {
-        this.userStorage.put(user.getId(), user);
-        return true;
+        return this.userStorage.putIfAbsent(user.getId(), user) == null;
     }
 
     public synchronized boolean update(User user) {
-        boolean result = false;
-        for (User u : this.userStorage.values()) {
-            if (u.getId() == user.getId()) {
-                u.setAmount(user.getAmount());
-                result = true;
-            }
-        }
-        return result;
+       return this.userStorage.replace(user.getId(), user) != null;
     }
 
     public synchronized boolean delete(User user) {
-        this.userStorage.remove(user.getId());
-        return true;
+        return this.userStorage.remove(user.getId(), user);
     }
 
     public synchronized void transfer(int fromId, int toId, int amount) {
-        this.userStorage.remove(fromId);
-        this.userStorage.replace(toId, this.userStorage.get(toId), new User(fromId, amount));
+        User userFrom = this.userStorage.get(fromId);
+        User userTo = this.userStorage.get(toId);
+        if(userFrom != null && userTo != null && userFrom.getAmount() >= amount) {
+            userFrom.setAmount(userFrom.getAmount() - amount);
+            userTo.setAmount(userTo.getAmount() + amount);
+        }
     }
 
     @Override
--- chapter_010/src/test/java/ru/job4j/jcip/UserStorageTest.java
+++ chapter_010/src/test/java/ru/job4j/jcip/UserStorageTest.java
@@ -3,7 +3,7 @@ package ru.job4j.jcip;
  * Chapter_010. 1. Multithreading[171#453877].
  * Task: 3. Класс хранилища пользователей UserStorage[1104#453906].
  * @author Andrei Kirillovykh (mailto:andykirill@gmail.com).
- * @version 2.
+ * @version 3.
  */
 import org.junit.Test;
 import static org.hamcrest.Matchers.is;
@@ -11,15 +11,27 @@ import static org.junit.Assert.assertThat;
 
 public class UserStorageTest {
     /**
-     * When Use User Storage.
+     * When Use User Storage Successful transfer.
      */
     @Test
-    public void whenUseUserStorage() {
+    public void whenSuccessfulTransfer() {
         UserStorage storage = new UserStorage();
         storage.add(new User(1, 100));
         storage.add(new User(2, 200));
         assertThat(storage.toString(), is("UserStorage{userStorage={1=User{id=1, amount=100}, 2=User{id=2, amount=200}}}"));
         storage.transfer(1, 2, 50);
-        assertThat(storage.toString(), is("UserStorage{userStorage={2=User{id=1, amount=50}}}"));
+        assertThat(storage.toString(), is("UserStorage{userStorage={1=User{id=1, amount=50}, 2=User{id=2, amount=250}}}"));
+    }
+    /**
+     * When Use User Storage Unsuccessful transfer.
+     */
+    @Test
+    public void whenUnsuccessfulTransfer() {
+        UserStorage storage = new UserStorage();
+        storage.add(new User(1, 100));
+        storage.add(new User(2, 200));
+        assertThat(storage.toString(), is("UserStorage{userStorage={1=User{id=1, amount=100}, 2=User{id=2, amount=200}}}"));
+        storage.transfer(1, 2, 150);
+        assertThat(storage.toString(), is("UserStorage{userStorage={1=User{id=1, amount=100}, 2=User{id=2, amount=200}}}"));
     }
 }
